bea68731a6abdeefa073bc11a3a40d92fe5879b4
gness1804 <graham.nessler@gmail.com>
2020-04-04 16:24:11 -0500

    WIP: transferred scratch.js to main file, but still an error

--- binary-search.ts
+++ binary-search.ts
@@ -19,4 +19,46 @@ export default class BinarySearch {
       ? arr
       : undefined;
   }
+
+  indexOf(targetNumber: number): number | undefined {
+    if (!this.array) return undefined;
+
+    if (!this.array.length) return undefined;
+
+    if (this.array.length === 1) return this.array.indexOf(targetNumber);
+
+    let startingIndex = 0;
+    let endingIndex = this.array.length - 1;
+    const originalArray = this.array;
+
+    const handleLoop = (newArr: number[]): number => {
+      const middleIndex = Math.ceil((newArr.length - 1) / 2);
+      const middleIndexNumber = newArr[middleIndex];
+
+      if (!newArr.includes(middleIndexNumber)) {
+        return -1;
+      }
+
+      if (!newArr.includes(targetNumber)) {
+        return -1;
+      }
+
+      if (middleIndexNumber === targetNumber) {
+        // we want the index of the found middleIndexNumber in the ORIGINAL array.
+        return originalArray.indexOf(middleIndexNumber);
+      }
+
+      if (middleIndexNumber < targetNumber) {
+        // we just want the back part of the array
+        startingIndex = newArr.indexOf(newArr[middleIndex + 1]);
+        return handleLoop([...newArr.slice(startingIndex, endingIndex)]);
+      } else {
+        // middleIndexNumber > targetNumber; we just want the front part of the array
+        endingIndex = newArr.indexOf(newArr[middleIndex - 1]);
+        return handleLoop([...newArr.slice(startingIndex, endingIndex)]);
+      }
+    };
+
+    return handleLoop(this.array);
+  }
 }
